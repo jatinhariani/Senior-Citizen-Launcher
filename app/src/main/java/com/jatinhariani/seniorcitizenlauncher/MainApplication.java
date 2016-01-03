298d04500d535396f7cd47ac45617d1d9d480570
Jatin Hariani <jtnh321@gmail.com>
2016-01-03 14:06:27 +0530

    dummy crash button and init fabric in mainapplication.

--- app/src/main/java/com/jatinhariani/seniorcitizenlauncher/MainApplication.java
+++ app/src/main/java/com/jatinhariani/seniorcitizenlauncher/MainApplication.java
@@ -2,6 +2,11 @@ package com.jatinhariani.seniorcitizenlauncher;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
+import io.fabric.sdk.android.Fabric;
+
 /**
  * Created by jatin on 03/01/16.
  */
@@ -10,5 +15,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        CrashlyticsCore core = new CrashlyticsCore.Builder().build();
+        Fabric.with(this, new Crashlytics.Builder().core(core).build());
     }
 }
