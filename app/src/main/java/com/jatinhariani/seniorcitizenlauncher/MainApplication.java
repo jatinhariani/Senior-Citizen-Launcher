9879685f7d533ec919234bb0ca0891bd892abdd0
Jatin Hariani <jtnh321@gmail.com>
2016-01-03 12:22:50 +0530

    added mainapplication.

---
+++ app/src/main/java/com/jatinhariani/seniorcitizenlauncher/MainApplication.java
@@ -0,0 +1,14 @@
+package com.jatinhariani.seniorcitizenlauncher;
+
+import android.app.Application;
+
+/**
+ * Created by jatin on 03/01/16.
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
